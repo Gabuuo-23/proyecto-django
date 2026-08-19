347ebec9e967a673948ceb56c3f9785f059151f9
gabuuo-23 <hisid18@gmail.com>
2026-08-19 16:21:27 -0600

    arreglo de bug de imagenes

--- prueba/prueba/settings.py
+++ prueba/prueba/settings.py
@@ -78,12 +78,23 @@ WSGI_APPLICATION = 'prueba.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+     'default': {
+         'ENGINE': 'django.db.backends.mysql',
+         'NAME': 'ejemplo',
+         'USER': 'gabo',
+         'PASSWORD': '123456',
+         'HOST': 'localhost',
+         'PORT': '3306',
+     }
+ }
+
 
 
 # Password validation
